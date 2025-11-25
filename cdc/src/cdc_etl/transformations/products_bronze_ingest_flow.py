a1661c1a133f32dcb6a1400eb7c78273af81e0aa
shaikapsar <shaik.apsar@hotmail.com>
2025-11-25 19:27:06 +0000

    cdc to clean

---
+++ cdc/src/cdc_etl/transformations/products_bronze_ingest_flow.py
@@ -0,0 +1,35 @@
+from pyspark import pipelines as dp
+from pyspark.sql.types import StructType, StructField, StringType
+
+dbutils.widgets.text(
+    "product_raw_dir",
+    defaultValue="/Volumes/ott/analytics/landing_zone/product/",
+    label="product_raw_dir"
+)
+
+product_raw_dir = dbutils.widgets.get("product_raw_dir")
+
+product_schema = StructType([
+    StructField("schema", StringType(), True),
+    StructField("payload", StringType(), True)
+])
+
+# Create the target bronze streaming table in ott.analytics schema
+dp.create_streaming_table(
+    name="products_cdc_bronze",
+    comment="Raw product CDC events incrementally ingested from cloud object storage"
+)
+
+@dp.append_flow(
+    target="products_cdc_bronze",
+    name="products_bronze_ingest_flow"
+)
+def products_bronze_ingest_flow():
+    return (
+        spark.readStream
+        .format("cloudFiles")
+        .option("cloudFiles.format", "json")
+        .option("cloudFiles.inferColumnTypes", "true")
+        .schema(product_schema)
+        .load(product_raw_dir)
+    )
